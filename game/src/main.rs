6ef4564af850538759c531b3d42a11ee26ded928
Aaron Kaswen-Wilk <aaron.kaswen-wilk@hellofresh.com>
2023-12-21 14:50:34 +0100

    refactor movement for adjusted diagonals

--- game/src/main.rs
+++ game/src/main.rs
@@ -3,6 +3,11 @@ use bevy::prelude::*;
 const SPRITE_SIZE: f32 = 100.0;
 const MOVE_SPEED: f32 = 500.0;
 
+#[derive(Component)]
+pub struct Player {
+    pub speed: f32,
+}
+
 fn main() {
     App::new()
         .add_plugins(
@@ -29,33 +34,63 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     let texture = asset_server.load("character.png");
 
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(SPRITE_SIZE, SPRITE_SIZE)),
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(SPRITE_SIZE, SPRITE_SIZE)),
+                ..default()
+            },
+            texture,
             ..default()
         },
-        texture,
-        ..default()
-    });
+        Player { speed: MOVE_SPEED },
+    ));
 }
 
 fn character_movement(
-    mut characters: Query<(&mut Transform, &Sprite)>,
+    mut characters: Query<(&mut Transform, &Player)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    for (mut transform, _) in &mut characters {
-        if input.pressed(KeyCode::W) || input.pressed(KeyCode::Up) {
-            transform.translation.y += MOVE_SPEED * time.delta_seconds();
+    for (mut transform, player) in &mut characters {
+        let mut movement_amount = player.speed * time.delta_seconds();
+
+        if up(&input) && (left(&input) || right(&input))
+            || down(&input) && (left(&input) || right(&input))
+        {
+            movement_amount = (((player.speed).powf(2.0))/2.0).sqrt() * time.delta_seconds();
         }
-        if input.pressed(KeyCode::S) || input.pressed(KeyCode::Down) {
-            transform.translation.y -= MOVE_SPEED * time.delta_seconds();
+
+        if up(&input) {
+            transform.translation.y += movement_amount;
         }
-        if input.pressed(KeyCode::A) || input.pressed(KeyCode::Left) {
-            transform.translation.x -= MOVE_SPEED * time.delta_seconds();
+
+        if down(&input) {
+            transform.translation.y -= movement_amount;
         }
-        if input.pressed(KeyCode::D) || input.pressed(KeyCode::Right) {
-            transform.translation.x += MOVE_SPEED * time.delta_seconds();
+
+        if left(&input) {
+            transform.translation.x -= movement_amount;
+        }
+
+        if right(&input) {
+            transform.translation.x += movement_amount;
         }
     }
+}
+
+fn up(input: &Res<Input<KeyCode>>) -> bool {
+    input.pressed(KeyCode::W) || input.pressed(KeyCode::Up)
+}
+
+fn down(input: &Res<Input<KeyCode>>) -> bool {
+    input.pressed(KeyCode::S) || input.pressed(KeyCode::Down)
+}
+
+fn left(input: &Res<Input<KeyCode>>) -> bool {
+    input.pressed(KeyCode::A) || input.pressed(KeyCode::Left)
+}
+
+fn right(input: &Res<Input<KeyCode>>) -> bool {
+    input.pressed(KeyCode::D) || input.pressed(KeyCode::Right)
 }
